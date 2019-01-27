423118f5c8bde6fddae5acdebd92e8fd5f35c8df
Sophearak Tha <t.sophearak@gmail.com>
2019-01-27 16:24:18 +0700

    update

--- src/index.js
+++ src/index.js
@@ -31,3 +31,43 @@ platforms.forEach(platform => {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'UA-133159807-2', { anonymize_ip: true });
+
+  const buttons = $$('.button');
+  buttons.forEach(button => {
+    button.addEventListener('click', btn => {
+      gtag('event', 'Click', {
+        event_category: 'CTA',
+        event_label: `Click ${btn.target.innerText}`,
+        value: btn.target.innerText
+      });
+    });
+  });
+
+  const links = $$('.nav--link a');
+  links.forEach(link => {
+    link.addEventListener('click', l => {
+      gtag('event', 'Click', {
+        event_category: 'Navigation',
+        event_label: `Click on ${l.target.innerText}`,
+        value: l.target.innerText
+      });
+    });
+  });
+
+  $('.badge').addEventListener('click', badge => {
+    gtag('event', 'Click', {
+      event_category: 'CTA',
+      event_label: `Click on badge version number`,
+      value: badge.target.innerText
+    });
+  });
+});
